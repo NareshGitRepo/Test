7adcee326caa97a42f94ebd8a773d0920b568c4c
NareshGitRepo <48349912+NareshGitRepo@users.noreply.github.com>
2019-07-19 18:06:36 +0530

    Translate Lang
    Dialogbox using ng-template and lang translate

--- Angular/src/app/app.component.spec.ts
+++ Angular/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'UploadFiles'`, () => {
+  it(`should have as title 'DialogBox'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('UploadFiles');
+    expect(app.title).toEqual('DialogBox');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to UploadFiles!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to DialogBox!');
   });
 });
--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -4,10 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {ReactiveFormsModule, FormsModule} from '@angular/forms';
-import {MatFormFieldModule, MatButtonModule, MatSnackBarModule, MatInputModule, MatSelectModule} from '@angular/material';
-import { NgxFileHelpersModule } from 'ngx-file-helpers';
-import { PapaParseModule } from 'ngx-papaparse';
+import {MatFormFieldModule, MatButtonModule, MatDialogModule, MatCheckboxModule, MatSlideToggleModule} from '@angular/material';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, '../assets/i18N/', '.json');
+}
+
+
 @NgModule({
   declarations: [
     AppComponent
@@ -16,15 +23,21 @@ import { PapaParseModule } from 'ngx-papaparse';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    ReactiveFormsModule,
-    FormsModule,
     MatFormFieldModule,
     MatButtonModule,
-    MatSnackBarModule,
-    MatInputModule,
-    NgxFileHelpersModule,
-    MatSelectModule,
-    PapaParseModule
+    MatDialogModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatCheckboxModule,
+    MatSlideToggleModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+        loader: {
+            provide: TranslateLoader,
+            useFactory: createTranslateLoader,
+            deps: [HttpClient]
+        }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
