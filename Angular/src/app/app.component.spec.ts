677ce9af8c21b06819d53978b40615e44c4333e8
NareshGitRepo <48349912+NareshGitRepo@users.noreply.github.com>
2019-07-24 19:08:47 +0530

    UploadFiles
    upload files format xls and txt with header conditions

--- Angular/src/app/app.component.spec.ts
+++ Angular/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'DialogBox'`, () => {
+  it(`should have as title 'UploadFiles'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('DialogBox');
+    expect(app.title).toEqual('UploadFiles');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to DialogBox!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to UploadFiles!');
   });
 });
--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -4,17 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatFormFieldModule, MatButtonModule, MatDialogModule, MatCheckboxModule, MatSlideToggleModule} from '@angular/material';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule, HttpClient } from '@angular/common/http';
-import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-
-export function createTranslateLoader(http: HttpClient) {
-  return new TranslateHttpLoader(http, '../assets/i18N/', '.json');
-}
-
-
+import {ReactiveFormsModule, FormsModule} from '@angular/forms';
+import {MatFormFieldModule, MatButtonModule, MatSnackBarModule, MatInputModule, MatSelectModule} from '@angular/material';
+import { NgxFileHelpersModule } from 'ngx-file-helpers';
+import { PapaParseModule } from 'ngx-papaparse';
 @NgModule({
   declarations: [
     AppComponent
@@ -23,21 +16,15 @@ export function createTranslateLoader(http: HttpClient) {
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    ReactiveFormsModule,
+    FormsModule,
     MatFormFieldModule,
     MatButtonModule,
-    MatDialogModule,
-    FormsModule,
-    ReactiveFormsModule,
-    MatCheckboxModule,
-    MatSlideToggleModule,
-    HttpClientModule,
-    TranslateModule.forRoot({
-        loader: {
-            provide: TranslateLoader,
-            useFactory: createTranslateLoader,
-            deps: [HttpClient]
-        }
-    })
+    MatSnackBarModule,
+    MatInputModule,
+    NgxFileHelpersModule,
+    MatSelectModule,
+    PapaParseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
